4820d2734dd27e1decb1fc50e1183f3eca08067c
kaustubh wade <kaustubhwade97@gmail.com>
2020-09-20 15:58:12 +0530

    Assignment 1

--- node-express/index.js
+++ node-express/index.js
@@ -4,6 +4,9 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
+
 const hostname = 'localhost';
 const port = 3000;
 
@@ -12,6 +15,8 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -24,5 +29,5 @@ app.use((req, res, next) => {
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
-    console.log(`Server runnign at http://${hostname}:${port}`);
+    console.log(`Server running at http://${hostname}:${port}`);
 });
